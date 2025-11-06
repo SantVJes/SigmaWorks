7d0f5b37f48d46b110924e33ca77c7e5fb5e917c
Jesus Santiago <sanjesvel@outlook.com>
2025-11-05 22:37:48 -0600

    feat: add Swagger documentation setup for Sigma Works API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Sigma Works API')
+    .setDescription(
+      'API del sistema **Sigma Works CRM**, desarrollada para pequeñas y medianas empresas.',
+    )
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
